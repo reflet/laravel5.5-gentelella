1de476e8aee3f58d592c3fa3f9199e133c67adf4
reflet <reflet.jp@gmail.com>
2019-03-23 14:09:23 +0900

    Bootstrap3のフリーテンプレート`Gentelella`を導入する
    ex) $ docker-compose run --rm node bash -c "yarn add gentelella --save && yarn production"
    
    素材(fonts, img)の追加漏れ

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -13,3 +13,14 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+/*
+ |--------------------------------------------------------------------------
+ | Bootstrap3 free admin template
+ |--------------------------------------------------------------------------
+ | https://github.com/ColorlibHQ/gentelella
+ */
+mix.copy('node_modules/gentelella/build/js/custom.js', 'public/js/gentelella.js')
+   .copy('node_modules/gentelella/build/css/custom.css', 'public/css/gentelella.css')
+   .copyDirectory('node_modules/gentelella/vendors', 'public/vendors')
+   .copyDirectory('node_modules/gentelella/build/images', 'public/img');
